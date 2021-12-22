2cba030037ce4ecd398e79c4998638b2da6ef6aa
Kaleb Olson <kaleb_olson@trimble.com>
2021-12-22 15:47:35 -0600

    Prepares desktop table for testing w/ api data

--- client/src/components/FileContainer.jsx
+++ client/src/components/FileContainer.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import Button from './Button'
 
 const FileContainer = ({ files }) => {
     const items = files.map((file) => {
-        
+        const showReviewBtn = (file.status === "Awaiting Client Review")
+        return (<tr>
+            <td><Button text={file.title} className={`file-link-btn ${file.link && 'btn-has-link'}`} btnLink={file.link}/></td>
+            <td>{file.stage}</td>
+            <td>{file.status}</td>
+            <td>{file.deadline}</td>
+            <td>{file.notes}</td>
+            {showReviewBtn && <td className='action-required-column'>Action Required by {file.deadline}!<Button text='Update Status' btnLink={file.reviewLink} className='file-review-btn'/></td>}
+        </tr>)
     })
 
     return (
+        <div className='file-container'>
         <table className='file-table'>
             <tr>
+                <th>File</th>
                 <th>Stage</th>
                 <th>Status</th>
-                <th>Action Required?</th>
-                <th>Deadline</th>
-                <th>Link</th>
+                <th>Review Deadline</th>
                 <th>Notes</th>
             </tr>
             {items}
         </table>
+        </div>
+
     )
 }
 

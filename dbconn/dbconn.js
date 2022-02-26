1cbc9de4f3096afdd078e6e582822a78e6579ed0
Kaleb Olson <kalebroyolson@gmail.com>
2022-02-25 22:43:21 -0600

    Mapped out dbcomm functions

--- dbconn/dbconn.js
+++ dbconn/dbconn.js
@@ -5,12 +5,50 @@ const region = process.env.region
 AWS.config.update({ region: region })
 let ddbClient = new AWS.DynamoDB.DocumentClient()
 
-function saveUser (creds) {
+async function saveUser (creds) {
+    const tableName = "CL-Users"
+    const params = {
+        TableName: tableName,
+        Item: {
+            "username": creds.username,
+            "passwordHash": creds.passwordHash,
+            "email": creds.email
+        }
+    }
 
+    return await ddbClient.put(params, (err, data) => {
+        return err || "Credentials added successfully"
+    })
+}
+
+function getUser (creds) {
+    const tableName = "CL-Users"
+    const params = {
+        TableName: tableName,
+        Keys: {
+            "username": creds.username
+        }
+    }
+
+    return await ddbClient.get(params, (err, data) => {
+        return err || data
+    })
 }
 
 function updateUserPassword (creds) {
+    const tableName = "CL-Users"
+    const params = {
+        TableName: tableName,
+        Item: {
+            "username": creds.username,
+            "passwordHash": creds.passwordHash,
+            "email": creds.email
+        }
+    }
 
+    return await ddbClient.put(params, (err, data) => {
+        return err || "Credentials added successfully"
+    })
 }
 
 module.exports = { saveUser, updateUserPassword }
